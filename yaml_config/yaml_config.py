3a614c3a630d187925efed9d7e7e7f01ea3c975a
Kazuma Sasaki <kazuma_sasaki@dwango.co.jp>
2019-09-27 02:11:57 +0900

    fix Path support

--- yaml_config/yaml_config.py
+++ yaml_config/yaml_config.py
@@ -3,17 +3,23 @@ import dataclasses
 import yaml
 import inspect
 
-yaml.add_representer(pathlib.Path, lambda dumper, instance: dumper.represent_str(str(instance)))
-yaml.add_constructor(pathlib.Path, lambda loader, node: pathlib.Path(node))
-
 
 @dataclasses.dataclass
 class YamlConfig:
     def save(self, config_path: pathlib.Path):
         """ Export config as YAML file """
         assert config_path.parent.exists(), f'directory {config_path.parent} does not exist'
+
+        def convert_dict(data):
+            for key, val in data.items():
+                if isinstance(val, pathlib.Path):
+                    data[key] = str(val)
+                if isinstance(val, dict):
+                    data[key] = convert_dict(val)
+            return data
+
         with open(config_path, 'w') as f:
-            yaml.dump(dataclasses.asdict(self), f)
+            yaml.dump(convert_dict(dataclasses.asdict(self)), f)
 
     @classmethod
     def load(cls, config_path: pathlib.Path):
@@ -21,15 +27,17 @@ class YamlConfig:
 
         assert config_path.exists(), f'YAML config {config_path} does not exist'
 
-        def convert_dict(parent_cls, data):
+        def convert_from_dict(parent_cls, data):
             for key, val in data.items():
                 child_class = parent_cls.__dataclass_fields__[key].type
+                if child_class == pathlib.Path:
+                    data[key] = pathlib.Path(val)
                 if inspect.isclass(child_class) and issubclass(child_class, YamlConfig):
-                    data[key] = child_class(**convert_dict(child_class, val))
+                    data[key] = child_class(**convert_from_dict(child_class, val))
             return data
 
         with open(config_path) as f:
             config_data = yaml.full_load(f)
             # recursively convert config item to YamlConfig
-            config_data = convert_dict(cls, config_data)
+            config_data = convert_from_dict(cls, config_data)
             return cls(**config_data)
