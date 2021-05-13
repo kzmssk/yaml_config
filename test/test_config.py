59d596c6f9c76b67ec6c42de3f3c0b148f1dbd29
hirachan <yo@hirano.cc>
2021-05-14 01:07:09 +0900

    convert recursively

--- test/test_config.py
+++ test/test_config.py
@@ -34,6 +34,16 @@ class MyConfigDict(YamlConfig):
     sub_sub_configs: typing.Dict[str, MySubSubConfig]
 
 
+@dataclasses.dataclass
+class MyConfigListInDict(YamlConfig):
+    sub_sub_configs: typing.Dict[str, typing.List[MySubSubConfig]]
+
+
+@dataclasses.dataclass
+class MyConfigDictInList(YamlConfig):
+    sub_sub_configs: typing.List[typing.Dict[str, MySubSubConfig]]
+
+
 def test_config():
 
     config = MyConfig(
@@ -87,5 +97,43 @@ def test_config_dict():
 
         # restore from YAML file
         config_load = MyConfigDict.load(config_path)
+
+        assert config == config_load
+
+
+def test_config_list_in_dict():
+
+    config = MyConfigListInDict(
+        sub_sub_configs={
+            "abc": [MySubSubConfig(val_float=3.0, val_list=[1, 2, 3, 4], val_str='5'),
+                    MySubSubConfig(val_float=13.0, val_list=[5, 6, 7, 8], val_str='9')]})
+
+    with tempfile.TemporaryDirectory() as d:
+        config_path = pathlib.Path(str(d)) / 'tmp.yaml'
+
+        # dump into YAML file
+        config.save(config_path)
+
+        # restore from YAML file
+        config_load = MyConfigListInDict.load(config_path)
+
+        assert config == config_load
+
+
+def test_config_dict_in_list():
+
+    config = MyConfigDictInList(
+        sub_sub_configs=[
+            {"abc": MySubSubConfig(val_float=3.0, val_list=[1, 2, 3, 4], val_str='5')},
+            {"def": MySubSubConfig(val_float=13.0, val_list=[5, 6, 7, 8], val_str='9')}])
+
+    with tempfile.TemporaryDirectory() as d:
+        config_path = pathlib.Path(str(d)) / 'tmp.yaml'
+
+        # dump into YAML file
+        config.save(config_path)
+
+        # restore from YAML file
+        config_load = MyConfigDictInList.load(config_path)
 
         assert config == config_load
